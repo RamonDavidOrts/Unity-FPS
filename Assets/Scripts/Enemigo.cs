8bfd6c148bf0d354f7e650a18b6830adb4ce82c4
Ramón Orts <rdorts@hotmail.com>
2021-05-07 11:37:23 +0200

    First commit

---
+++ Assets/Scripts/Enemigo.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemigo : MonoBehaviour
+{
+    [SerializeField] Transform objetivo;
+    NavMeshAgent agente;
+    float velocidad = 2;
+    float distanciaCambio = 0.2f;
+    byte siguientePosicion = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agente = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        agente.SetDestination(objetivo.position);
+    }
+}
